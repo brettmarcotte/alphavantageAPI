254c5d77c57ce53439c9a697443ccf113409459d
brettmarcotte <87341291+brettmarcotte@users.noreply.github.com>
2022-06-07 14:34:39 -0700

    Add files via upload

--- OverviewRepository.java
+++ OverviewRepository.java
@@ -10,6 +10,18 @@ public interface OverviewRepository extends CrudRepository<Overview, Long> {
 
     Optional<Overview> findBySymbol(String symbol);
 
+    Optional<Overview> findByAssetType(String assetType);
+
+    Optional<Overview> findByName(String name);
+
     List<Overview> findByExchange(String exchange);
+
+    List<Overview> findByCurrency(String currency);
+
+    List<Overview> findByCountry(String country);
+
+    List<Overview> findBySector(String Sector);
+
+
     
 }
