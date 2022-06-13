9e098fde736f1bfbf7e6df5b75509480a3848ada
brettmarcotte <87341291+brettmarcotte@users.noreply.github.com>
2022-06-13 11:46:58 -0700

    Add files via upload

--- OverviewRepository.java
+++ OverviewRepository.java
@@ -22,7 +22,25 @@ public interface OverviewRepository extends JpaRepository<Overview, Long> {
 
     public List<Overview> findByCountry(String country);
 
+    public List<Overview> findByMarketCapGreaterThanEqual(long marketCap);
+
+    public List<Overview> findByMarketCapLessThanEqual(long marketCap);
+
+
+
     public List<Overview> deleteById(long id);
 
     public List<Overview> deleteBySymbol(String symbol);
+
+    public List<Overview> deleteBySector (String sector);
+
+    public List<Overview> deleteByName(String name);
+
+    public List<Overview> deleteByCurrency(String currency);
+
+    public List<Overview> deleteByCountry(String country);
+
+    public List<Overview> deleteByMarketCapGreaterThanEqual(long marketCap);
+
+    public List<Overview> deleteByMarketCapLessThanEqual(long marketCap);
 }
